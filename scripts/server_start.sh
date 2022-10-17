dcce5b725a46906c72e255503b5b64b1dd2a1140
Vigneshnakkeeran <110113871+Vigneshnakkeeran@users.noreply.github.com>
2022-10-17 23:25:24 +0530

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo chmod +x /opt/tomcat/bin/./startup.sh
-sudo /opt/tomcat/bin/./startup.sh
+sudo chmod +x /root/opt/apache-tomcat-9.0.68/bin/./startup.sh
+sudo /root/opt/apache-tomcat-9.0.68/bin/./startup.sh
